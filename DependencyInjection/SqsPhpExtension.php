8b15b760765e0d3dcf6419fe94756d79e65d0290
Albert Hilazo <albhilazo@gmail.com>
2016-03-28 13:09:28 +0200

    Added extension to use bundle services

---
+++ DependencyInjection/SqsPhpExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SqsPhpBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+
+
+
+class SqsPhpExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/services')
+        );
+        $loader->load('services.yml');
+    }
+
+}
